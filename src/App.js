b7bb04367403938fb3f1db4cc11cf5ed2be593a0
Wendy Chu <seasonalrain@gmail.com>
2019-10-05 19:09:18 -0400

    initial puppy

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 
+import styled from 'styled-components'
+
+const Page = styled.div`
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 100vw;
+  height: 100vh;
+
+`
+
+const DogFrame = styled.img`
+  max-height: 60vh;
+
+`
 
 const App = () => {
   return (
-    <div className="App">
-      
-    </div>
+    <Page>
+      <DogFrame src="https://s.abcnews.com/images/Lifestyle/puppy-ht-3-er-170907_4x3_992.jpg"/>
+    </Page>
   )
 }
 
