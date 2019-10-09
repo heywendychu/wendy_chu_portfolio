61bec74943361d2247e46bc781de5d3e24017962
IanGrubb <hi@iangrubb.com>
2019-10-09 12:42:19 -0400

    project page routing structure

--- src/Templates/Project.js
+++ src/Templates/Project.js
@@ -6,14 +6,35 @@ const Container = styled.div`
 
 `
 
-const Project = props => {
-    console.log("dynamic route:", props.match.params.name)
-    return (
+
+const choosePage = route => {
+
+    const data = [
+    {route: "template",
+    password: null,
+    content:
         <Container>
             <h1>Project</h1>
             
         </Container>
-    )
+    }
+    ]
+
+    const selected = data.find( d => d.route === route)
+
+    return selected ? selected.content : (
+        <Container>
+            <h1>404</h1>
+        </Container>)
+
+
+
+}
+
+
+const Project = props => {
+    console.log("dynamic route:", props.match.params.name)
+    return choosePage(props.match.params.name)
 }
 
 
