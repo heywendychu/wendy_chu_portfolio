39a9a4d21680c0cc6c8aabec7fb13965c7b203aa
Wendy Chu <seasonalrain@gmail.com>
2019-10-13 15:21:56 -0400

    made span link underlines box shadows

--- src/UserInterface/SpanLink.js
+++ src/UserInterface/SpanLink.js
@@ -8,13 +8,15 @@ const Container = styled.a`
     cursor: pointer;
 
     color: #1b2c63;
-    text-decoration-color: rgba(33, 53, 113, 0.5);
-
+    // text-decoration-color: rgba(33, 53, 113, 0.5);
+    text-decoration: none;
+    box-shadow: 0 1px 0 rgba(33, 53, 113, 0.26);
     font-weight: 700;
 
     :hover {
         color: #de3c2c;
         text-decoration: none;
+        box-shadow: none;
     }
 
 `
