4fa49d876ddfaf64cde1458a96dfb631b45f1b67
Hannah Shore <shore.hannah.e@gmail.com>
2025-07-07 17:09:17 -0700

    Format JS files.

--- src/UserInterface/ImageGroup.js
+++ src/UserInterface/ImageGroup.js
@@ -1,58 +1,53 @@
-import React from 'react'
+import React from "react";
 
-import styled from 'styled-components'
-
-
+import styled from "styled-components";
 
 // Two images: one desktop, one mobile. Both full width, auto height. Optional caption.
 
 const Container = styled.div`
-    width: 100%;
-`
+  width: 100%;
+`;
 
 const Picture = styled.img`
+  width: 100%;
+  height: auto;
 
-    width: 100%;
-    height: auto;
+  margin: 40px 0 0 0;
 
-    margin: 40px 0 0 0;
-
-    display: ${props => props.toggle === "mobile" ? "none" : "block"}
-
-
-    @media (max-width: 768px) {
-
-        display: ${props => props.toggle === "desktop" ? "none" : "block"}
-
-    }
-
-
-`
+  display: ${(props) => (props.toggle === "mobile" ? "none" : "block")} @media
+    (max-width: 768px) {
+    display: ${(props) => (props.toggle === "desktop" ? "none" : "block")};
+  }
+`;
 
 const Caption = styled.div`
+  margin: 16px 0 24px 0;
 
-    margin: 16px 0 24px 0;
+  font-family: "Roboto Mono", monospace;
+  font-size: 12px;
+  line-height: 1.67;
+  text-align: center;
+  color: #1b2c63;
+`;
 
-    font-family: 'Roboto Mono', monospace;
-    font-size: 12px;
-    line-height: 1.67;
-    text-align: center;
-    color: #1b2c63;
+const ImageGroup = (props) => {
+  return (
+    <Container>
+      <Picture
+        toggle={props.mobile ? "desktop" : null}
+        src={`/images/${props.desktop}`}
+        alt={props.alt}
+      />
+      {props.mobile ? (
+        <Picture
+          toggle={"mobile"}
+          src={`/images/${props.mobile}`}
+          alt={props.alt}
+        />
+      ) : null}
+      <Caption>{props.caption}</Caption>
+    </Container>
+  );
+};
 
-`
-
-
-
-const ImageGroup = props => {
-    return (
-        <Container>
-
-            <Picture toggle={props.mobile ? "desktop" : null} src={`/images/${props.desktop}`} alt={props.alt} />
-            {props.mobile ? <Picture toggle={"mobile"} src={`/images/${props.mobile}`} alt={props.alt} /> : null }
-            <Caption>{props.caption}</Caption>
-            
-        </Container>
-    );
-}
-
-export default ImageGroup
+export default ImageGroup;
